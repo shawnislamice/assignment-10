6d448112a8a5dbd9e4b6ed04ea020fefb796524d
Shaw Islam <shawnislam.ice@gmail.com>
2024-04-30 18:19:55 +0600

    Some things updated

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,3 +1,4 @@
+import { Typewriter } from 'react-simple-typewriter';
 import slider1 from '../assets/slider1.jpg'
 import slider2 from '../assets/slider2.jpg'
 import slider3 from '../assets/slider3.avif'
@@ -5,6 +6,23 @@ import slider4 from '../assets/slider4.jpg'
 const Slider = () => {
   return (
     <div className="px-4 container mx-auto max-w-screen-xl md:my-10">
+      <hr className="my-2 max-w-6xl mx-auto border border-dashed" />
+      <h2 className="text-center text-xl md:text-3xl font-bold">
+        {" "}
+        <Typewriter
+          words={["Some Awesome Photo Taken By Our Happy Travellers", "Photos"]}
+          loop={true}
+          cursor
+          cursorStyle="_"
+          typeSpeed={100}
+          deleteSpeed={50}
+          delaySpeed={1000}
+        />
+      </h2>
+      <p className="text-center opacity-90 md:pt-2 max-w-md mx-auto">
+        Some popular destination where our travellers already visited and share their experiences
+      </p>
+      <hr className="my-2 max-w-6xl mx-auto border border-dashed" />
       <div className="carousel w-full">
         <div id="slide1" className="carousel-item relative w-full">
           <img
